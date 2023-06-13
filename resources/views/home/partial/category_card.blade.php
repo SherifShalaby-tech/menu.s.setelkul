d73dbef63d1ed3b437225eeee0ff81042229a28a
eitharSaeed <eithar.saeed256@gmail.com>
2023-06-13 10:36:16 +0300

    edit xxs mobile design

--- resources/views/home/partial/category_card.blade.php
+++ resources/views/home/partial/category_card.blade.php
@@ -2,7 +2,7 @@
     class="w-full h-0 shadow-lg pb-full rounded-xl bg-center bg-no-repeat bg-cover relative border-2 border-dark mb:mb-8 xs:mb-16"
     style="background-image: url('{{ !empty($category->getFirstMediaUrl('product_class')) ? images_asset($category->getFirstMediaUrl('product_class')) : images_asset(asset('uploads/' . session('logo'))) }}')">
     <div class="flex absolute md:-bottom-16 xs:-bottom-10 w-full">
-        <div class="lg:h-10 md:h-8 xs:h-6 md:w-48 xs:w-40 bg-darkblue mx-auto text-center rounded-3xl">
+        <div class="lg:h-10 md:h-8 xs:h-6 md:w-48 xs:w-40 bg-darkblue mx-auto text-center rounded-3xl category">
             <h3 class="lg:text-xl md:text-sm xs:text-tiny text-white font-semibold py-1">{{ $category->name }}</h3>
         </div>
     </div>
